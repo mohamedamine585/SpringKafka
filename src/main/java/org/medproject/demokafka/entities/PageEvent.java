fb3b0996511faa2864d208e894f6b5a11dfd5de6
Mohamed amine Tlili <foxweb585@gmail.com.com>
2024-04-30 09:15:20 +0100

    +

---
+++ src/main/java/org/medproject/demokafka/entities/PageEvent.java
@@ -0,0 +1,17 @@
+package org.medproject.demokafka.entities;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+import  java.util.Date;
+@Data @NoArgsConstructor @AllArgsConstructor @ToString
+public class PageEvent {
+
+    private String name;
+    private  String user ;
+    private  Date date;
+    private  long duration;
+
+}
